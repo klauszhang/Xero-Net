b0fca0705bc86c0ae13ec69f079f63971ea3c305
klauszhang <klaushaozhang.note@gmail.com>
2017-07-12 15:10:00 +1200

    add validation error including order id and customer name fields

--- Xero.Api/Infrastructure/Model/ValidationError.cs
+++ Xero.Api/Infrastructure/Model/ValidationError.cs
@@ -2,10 +2,16 @@
 
 namespace Xero.Api.Infrastructure.Model
 {
-    [DataContract(Namespace = "")]
-    public class ValidationError
-    {
-        [DataMember(EmitDefaultValue = false)]
-        public string Message { get; set; }
-    }
+  [DataContract(Namespace = "")]
+  public class ValidationError
+  {
+    [DataMember(EmitDefaultValue = false)]
+    public string Message { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public string OrderId { get; set; }
+
+    [DataMember(EmitDefaultValue = false)]
+    public string CustomerName { get; set; }
+  }
 }
